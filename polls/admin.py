6c49c8f92cdd292bbf744502b84a780752ab0d25
Ryan Scarbel <ryanscarbel@gmail.com>
2021-11-25 12:01:32 -0500

    Added admin choice

--- polls/admin.py
+++ polls/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Question
+from .models import Question, Choice
 
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
@@ -9,4 +9,5 @@ class QuestionAdmin(admin.ModelAdmin):
     ]
 
 admin.site.register(Question)
+admin.site.register(Choice)
 
